1c797215ad038a1dc234f230b70396fff6f24520
oscarhilton <olhilton@gmail.com>
2023-09-30 01:59:47 +0000

    cors

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 import eyed3
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"methods": ["GET", "POST"]}})
 
 
 @app.route('/')
